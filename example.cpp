bdf8a8e82237e32fb7ef19125868f72730585b37
Fred Emmott <fred@fredemmott.com>
2020-12-19 08:53:11 -0600

    add license headers

--- example.cpp
+++ example.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020-present, Fred Emmott <fred@fredemmott.com>
+ * All rights reserved.
+ *
+ * This source code is licensed under the ISC license found in the LICENSE file
+ * in the root directory of this source tree.
+ */
 #include "easymode.h"
 
 int main() {
