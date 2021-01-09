dbab4f2accf6b69429a480a15e32fe7911464585
Fred Emmott <fred@fredemmott.com>
2021-01-09 13:20:03 -0600

    Revert "Intentional test breakage"
    CI failed as expected, so let's unbreak things :)
    
    This reverts commit 6e42f25ecb03786d84a950ece4f03827574b213a.

--- lib/squaredeadzone.cpp
+++ lib/squaredeadzone.cpp
@@ -5,9 +5,8 @@
  * This source code is licensed under the ISC license found in the LICENSE file
  * in the root directory of this source tree.
  */
+#define _NO_MIN_MAX
 #include "squaredeadzone.h"
-
-#include <cmath>
 
 namespace fredemmott::inputmapping::actions {
 
@@ -20,18 +19,22 @@ SquareDeadzone::SquareDeadzone(
 SquareDeadzone::~SquareDeadzone() {}
 
 void SquareDeadzone::map(long value) {
-  // Recenter around (0, 0), then scale. The original mid
-  // point is now the max
-  const long MID = 0x7fff;
-  value -= MID;
-  const long boundary = (MID * mPercent) / 100;
-  if (abs(value) < boundary) {
-    mNext->map(MID);
+  // Re-scale to 100x to avoid dividing by 100 all over the place when dealing
+  // with percentages
+  value *= 100;
+  const uint32_t MAX = 0xffff * 100;
+  const uint32_t MID = MAX / 2;
+  const uint32_t DEAD = (mPercent * MID) / 100;
+  if (value > (MID - DEAD) && value < (MID + DEAD)) {
+    mNext->map(MID / 100);
     return;
   }
-  const long new_scale = MID - boundary;
-  const long scaled = ((value > 0 ? value - boundary : value + boundary) * MID) / new_scale;
-  mNext->map(scaled + MID); // re-center to (MID, MID)
+  const uint32_t NEW_MAX = MAX - (2 * DEAD);
+  const uint32_t without_deadzone = (value < MID) ? value : value - (2 * DEAD);
+  // We end up with a temporary where the value is multiplied by 100 * MAX
+  // * NEW_MAX; if the percentage is large, this overflows the uint32
+  const uint32_t scaled = (without_deadzone * (uint64_t) MAX) / NEW_MAX;
+  mNext->map(scaled / 100);
 }
 
 } // namespace fredemmott::inputmapping::actions
