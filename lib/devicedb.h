2644fc1ca4dde9dc4e3025ce755fcc76e3b9a027
Fred Emmott <fred@fredemmott.com>
2021-11-25 16:51:26 -0600

    Add VPC MT50CM2 stick to devicedb

--- lib/devicedb.h
+++ lib/devicedb.h
@@ -15,4 +15,5 @@ const VIDPID TM_TPR {0x044f, 0xb68f};
 const HardwareID VPC_LEFT_WARBRD {"HID\\VID_3344&PID_80CC&Col01"};
 const HardwareID VPC_RIGHT_WARBRD {"HID\\VID_3344&PID_40CC&Col01"};
 const HardwareID VPC_MT50CM3_THROTTLE {"HID\\VID_3344&PID_0194&Col01"};
+const HardwareID VPC_MT50CM2_STICK {"HID\\VID_3344&PID_012E&Col01"};
 }// namespace fredemmott::inputmapping::devicedb
