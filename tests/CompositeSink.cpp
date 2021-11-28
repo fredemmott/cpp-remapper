34996f11c35702eaeab7206fa370a92a9e7f6ab4
Fred Emmott <fred@fredemmott.com>
2021-11-28 14:45:50 -0600

    Test CompositeSink runtime behavior

--- tests/CompositeSink.cpp
+++ tests/CompositeSink.cpp
@@ -17,40 +17,22 @@ using namespace fredemmott::inputmapping;
 TEST_CASE("CompositeSink") {
   TestAxis axis;
 
-  // For now, relying on static_test below
-#ifdef TODO
   Axis::Value out1(-1), out2(-1), out3(-1);
   // 1, 2, n are special
-  axis >> &out1;
+  axis >> all([&](Axis::Value v) { out1 = v; });
   axis.emit(Axis::MAX);
   REQUIRE(out1 == Axis::MAX);
-  axis >> ::fredemmott::inputmapping::all(&out1, &out2);
+  axis
+    >> all([&](Axis::Value v) { out1 = v; }, [&](Axis::Value v) { out2 = v; });
   axis.emit(Axis::MIN);
   REQUIRE(out1 == Axis::MIN);
   REQUIRE(out2 == Axis::MIN);
-  axis >> ::fredemmott::inputmapping::all(&out1, &out2, &out3);
+  axis >> all(
+    [&](Axis::Value v) { out1 = v; },
+    [&](Axis::Value v) { out2 = v; },
+    [&](Axis::Value v) { out3 = v; });
   axis.emit(Axis::MAX);
   REQUIRE(out1 == Axis::MAX);
   REQUIRE(out2 == Axis::MAX);
   REQUIRE(out3 == Axis::MAX);
-#endif
 }
-
-namespace {
-
-void static_test() {
-  ButtonSinkRef b = [](bool) {};
-  auto single_button = all(b);
-  auto two_buttons = all(b, b);
-  auto many_buttons = all(b, b, b, b, b);
-
-  auto lambda = all([](bool) {});
-  auto many_lambdas = all([](bool) {}, [](bool) {}, [](bool) {});
-  static_assert(std::same_as<CompositeSink<Button>, decltype(many_lambdas)>);
-
-  auto many_axis_lambdas = all([](long) {}, [](long) {}, [](long) {});
-  static_assert(!std::same_as<CompositeSink<Axis>, decltype(many_lambdas)>);
-  static_assert(std::same_as<CompositeSink<Axis>, decltype(many_axis_lambdas)>);
-}
-
-}// namespace
